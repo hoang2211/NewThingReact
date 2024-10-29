82b00388dee6939b20ca7e79f25496decd00a363
ZoekLis <hoangnntde170445@fpt.edu.vn>
2024-10-29 16:29:20 +0700

    Add footer,searchbar,collection, filter

--- website/front-end/src/context/ShopContext.jsx
+++ website/front-end/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
@@ -7,9 +7,12 @@ const ShopContextProvider = (props) =>{
     
     const currency = '$';
     const delivery_fee = 10;
+    const [search,setSearch] =useState('');
+    const [showSearch,setShowSearch] = useState(false);
 
     const value ={
         products,currency,delivery_fee,
+        search,setSearch,showSearch,setShowSearch,
     }
 
     return (
@@ -17,6 +20,6 @@ const ShopContextProvider = (props) =>{
             {props.children}
         </ShopContext.Provider>
     )
-}
+}   
 
 export default ShopContextProvider;
--- website/front-end/src/pages/Home.jsx
+++ website/front-end/src/pages/Home.jsx
@@ -4,15 +4,19 @@ import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
 import NewLetterBox from '../components/NewLetterBox'
+import Footer from '../components/Footer'
+import SearchBar from '../components/SearchBar'
 
 const Home = () => {
   return (
     <div>
       <Hero  />
+      <SearchBar />
       <LatestCollection />
       <BestSeller />
       <OurPolicy />
       <NewLetterBox />
+      <Footer />
     </div>
   )
 }
